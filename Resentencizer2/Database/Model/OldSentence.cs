8cacbe80303cbc92c1665574ea3eb5e382a2ed2e
suflors <iammagical.space@gmail.com>
2024-07-22 03:25:15 -0700

    Added OldSentence.

---
+++ Resentencizer2/Database/Model/OldSentence.cs
@@ -0,0 +1,14 @@
+﻿namespace Resentencizer2.Database.Model
+{
+	public record OldSentence(
+		ulong MessageID,
+		int FragmentNumber,
+		ulong UserID,
+		ulong ChannelID,
+		ulong ServerID,
+		string Text,
+		int VersionNumber,
+		bool Deactivated = false,
+		bool InWordTable = false
+	);
+}
