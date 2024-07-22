df2c09864d6d0441a6742e727d14e5f4fa4b2cff
suflors <iammagical.space@gmail.com>
2024-07-22 03:28:56 -0700

    Added ResentencizerOptions.

---
+++ Resentencizer2/ResentencizerOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Resentencizer2
+{
+	public class ResentencizerOptions
+	{
+		public string OldConnectionString { get; set; } = "YOU NEED TO SET THIS!";
+		public int BatchSize { get; set; } = 50;
+		public int CurrentVersion { get; set; }
+	}
+}
