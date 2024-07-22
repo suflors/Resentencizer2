51f0e2b45799d020428c54803ad2e76d8add3504
suflors <iammagical.space@gmail.com>
2024-07-22 03:53:49 -0700

    Added ReadOldSentence().

--- Resentencizer2/Database/SqliteOldSentenceAccess.cs
+++ Resentencizer2/Database/SqliteOldSentenceAccess.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.Options;
+﻿using Dapper;
+using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Options;
+using Resentencizer2.Database.Model;
 
 namespace Resentencizer2.Database
 {
@@ -9,6 +12,27 @@ namespace Resentencizer2.Database
 		public SqliteOldSentenceAccess(IOptions<ResentencizerOptions> options)
 		{
 			this.options = options.Value;
+		}
+
+		public async Task<IEnumerable<OldSentence>> ReadOldSentence()
+		{
+			await using var connection = new SqliteConnection(options.OldConnectionString);
+			connection.Open();
+
+			var result = await connection.QueryAsync<OldSentence>($@"
+SELECT * FROM Sentence
+WHERE {nameof(OldSentence.VersionNumber)} < @currentVersion
+GROUP BY {nameof(OldSentence.MessageID)}
+LIMIT @batchSize",
+			new
+			{
+				batchSize = options.BatchSize,
+				currentVersion = options.CurrentVersion
+			});
+
+			connection.Close();
+
+			return result;
 		}
 	}
 }
