c86acd2ff0e1347ee2c38ecf291f16f78836ae36
suflors <iammagical.space@gmail.com>
2024-07-22 03:33:38 -0700

    Added SqliteOldSentenceAccess with constructor.

---
+++ Resentencizer2/Database/SqliteOldSentenceAccess.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace Resentencizer2.Database
+{
+	public class SqliteOldSentenceAccess
+	{
+		private readonly ResentencizerOptions options;
+
+		public SqliteOldSentenceAccess(IOptions<ResentencizerOptions> options)
+		{
+			this.options = options.Value;
+		}
+	}
+}
