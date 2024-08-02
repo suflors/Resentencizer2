9bb0ca688773ab0d220c7e2b7f833da870b7f71a
suflors <iammagical.space@gmail.com>
2024-08-02 15:01:59 -0700

    Service setup, is able to access Discord.

--- Resentencizer2/Program.cs
+++ Resentencizer2/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Discord.Rest;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Resentencizer2.Database;
+
+namespace Resentencizer2
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			CreateHostBuilder(args).Build().Run();
+		}
+
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureAppConfiguration((hostContext, builder) =>
+				{
+					// Add other providers for JSON, etc.
+
+					builder.AddUserSecrets<ResentencizerService>();
+				})
+				.ConfigureServices((hostContext, services) =>
+				{
+					services.AddHostedService<ResentencizerService>()
+							.AddSingleton<DiscordRestClient>()
+							.AddSingleton<SqliteOldSentenceAccess>();
+					services.AddOptions<ResentencizerOptions>()
+							.Bind(hostContext.Configuration.GetSection("Resentencizer"));
+				});
+	}
+}
---
+++ Resentencizer2/ResentencizerService.cs
@@ -0,0 +1,61 @@
+﻿using Discord;
+using Discord.Rest;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace Resentencizer2
+{
+	public class ResentencizerService : IHostedService
+	{
+		private readonly IConfiguration configuration;
+		private readonly DiscordRestClient client;
+
+		public ResentencizerService(IConfiguration configuration, DiscordRestClient client)
+		{
+			this.configuration = configuration;
+			this.client = client;
+		}
+
+		public async Task StartAsync(CancellationToken cancellationToken)
+		{
+			Console.WriteLine("starting ...");
+
+			client.Log += Log;
+
+			var token = configuration["Discord-API-Key"];
+
+			await client.LoginAsync(TokenType.Bot, token);
+
+			Console.WriteLine("hello");
+
+			//await Looper();
+		}
+
+		private async Task Looper()
+		{
+			while (true)
+			{
+				await Task.WhenAll(Resentencizer2(), Task.Delay(TimeSpan.FromMinutes(1)));
+			}
+		}
+
+		private async Task Resentencizer2()
+		{
+			var channel = await client.GetChannelAsync(1226673059314794638) as IMessageChannel;
+			var message = await channel.GetMessageAsync(1268977833636528150);
+			Console.WriteLine(message);
+		}
+
+		public async Task StopAsync(CancellationToken cancellationToken)
+		{
+			await client.LogoutAsync();
+			Console.WriteLine("goodbye");
+		}
+
+		private Task Log(LogMessage msg)
+		{
+			Console.WriteLine(msg.ToString());
+			return Task.CompletedTask;
+		}
+	}
+}
